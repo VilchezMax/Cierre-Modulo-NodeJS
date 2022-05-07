254c2440a5e37d1b26205f3f82803069b7d8ab3b
VilchezMax <maximiliano.vilchez@outlook.com>
2022-05-07 17:55:38 -0300

    Ejercicio terminado

---
+++ autoss.js
@@ -0,0 +1,55 @@
+function Auto(marca,modelo,color,anio,km,precio,cuotas,patente,vendido){
+    this.marca=marca;
+    this.modelo=modelo;
+    this.color=color;
+    this.anio=anio;
+    this.km=km;
+    this.precio=precio;
+    this.cuotas=cuotas;
+    this.patente=patente;
+    this.vendido=vendido;
+}
+
+let auto1 = new Auto("Ford","Fiesta","Azul",2019,200,150000,12,"APL123",false);
+let auto2 = new Auto("Toyota","Corolla","Blanco",2019,0,100000,14,"JJH116",false)
+
+let autos = [
+    auto1,auto2
+    ]
+//----------------------------------------------------------
+
+let concesionaria = {
+
+    autos : autos, 
+    buscarAuto: function(patenteBuscada){
+        let autoEncontrado=null;
+        this.autos.filter( auto => auto.patente==patenteBuscada ? autoEncontrado=auto : "");
+       
+        return autoEncontrado;
+    },
+
+    venderAuto: function(patenteBuscada){
+        autoAVender = this.buscarAuto(patenteBuscada);
+        autoAvender = concesionaria.autos.find(elemento=> {
+            return elemento.patente === "JJH116"
+        })
+        autoAVender.vendido=true;
+        
+        return autoAVender;
+    },
+
+    autosParaLaVenta: function(){
+        let arrayVendibles = this.autos.filter(auto => auto.vendido==false)
+           
+        return arrayVendibles
+    },
+
+   autosNuevos: function(){
+       let autos0km = [];
+       this.autosParaLaVenta().forEach(element => {
+           element.km<100 ? autos0km.push(element): ""
+       });
+       return autos0km;
+   }
+}
+
